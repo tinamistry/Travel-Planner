58bb92e3ff985f6761f4b13ed7269c3984fc575e
tinamistry <tmistry10@gmail.com>
2025-01-29 19:56:39 -0500

    added react routes for the create trip page

--- ai-travel-planner/src/main.jsx
+++ ai-travel-planner/src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router'
+import CreateTrip from './create-trip'
 
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>
+  },
+  {
+    path:'/create-trip',
+    element:<CreateTrip/>
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router}></RouterProvider>
   </StrictMode>,
 )
