de95cf3f4f26bc395b675e837b45cfb4508b93da
tinamistry <tmistry10@gmail.com>
2025-04-20 15:24:28 -0400

    Saved trips to firebase

---
+++ ai-travel-planner/src/view-trip/[tripId]/components/InfoSection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+
+function InfoSection({trip}){
+    return(
+        <div>
+
+            <img src = '/placeholder.jpg'/>
+            
+        </div>
+    )
+}
+
+export default InfoSection
--- ai-travel-planner/src/view-trip/[tripId]/index.jsx
+++ ai-travel-planner/src/view-trip/[tripId]/index.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import { db } from '@/service/fireBaseConfig';
+import { doc , getDoc } from 'firebase/firestore';
+import { toast } from '@/hooks/use-toast';
+import { useEffect , useState } from 'react';
+import InfoSection from './components/InfoSection';
 
 function Viewtrip(){
+
+    const{tripId} = useParams();
+    const [trip, setTrip] = useState([])
+
+    useEffect(()=>{
+        tripId&&GetTripData();
+    },[tripId])
+
+    const GetTripData=async()=>{
+        const docRef = doc(db, 'AITrips', tripId)
+        const docSnap = await getDoc(docRef)
+
+        if(docSnap.exists()){
+            console.log("Document", docSnap.data());
+            setTrip(docSnap.data())
+        }
+        else{
+            console.log("no search document")
+            toast('No Trip Found!')
+        }
+    }
+    
+
     return(
-        <div>view trip</div>
+
+        <div>
+            <InfoSection trip = {trip}/>
+         </div>
     )
 }
 
